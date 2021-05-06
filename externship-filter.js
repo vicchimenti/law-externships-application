cc9954d8f0788198252ee3021c9f31cd7cc3f86c
Vic Chimenti <victor.chimenti@gmail.com>
2021-05-06 12:35:30 -0700

    import dropdown logic for filter engine

--- externship-filter.js
+++ externship-filter.js
@@ -1,13 +1,13 @@
-<script type="text/javascript">
+<script>
 /***
 *   @author Victor Chimenti, MSCS-SE '20
-*   @file profile-filter.js
+*   @file article-filter.js
 *
 *   jQuery
-*   This script searches the Law School faculty profile content items for matches to the
-*   user selected search parameters in the filter field dropdown menus
+*   This script searches the Gadget Finder Article content items for matches to the
+*   user selected search parameters in the filter field dropdown menu
 *
-*   @version 3.0
+*   @version 3.3
 */
 
 
@@ -15,99 +15,94 @@
 
 
 
-    $(function () {
-        // After the DOM is ready, Wait until the window loads
-        $(window).load(function () {
-            // Once window loads set a timeout delay
-            setTimeout(function () {
-                console.log("timeout function");
+$(function () {
+    // After the DOM is ready, Wait until the window loads
+    $(window).load(function () {
+        // Once window loads set a timeout delay
+        setTimeout(function () {
 
 
 
 
-
-                //** global array holds list of content items that will render after filter selection **//
-                var visibleItems = [];
-                var parseItems = {};
-
+            //** global array holds list of content items that will render after filter selection **//
+            var visibleItems = [];
+            var parseItems = {};
 
 
 
-                //   ***   Process and Parse Visible Items   ***   //
-                $(function () {
-                    let parseItemsToDisplay = function () {
-                        // assign array of currently visible content items
-                        visibleItems = $('.lawFacultyWrapper').not('.hideByText, .hideByType');
-                        // check to see if array is empty
-                        if (visibleItems.length == 0) {
-                            // when array is empty show the results message
-                            $('.noResultsToShow').removeClass('hideResultsMessage');
-                        } else {
-                            // when array has content items suppress the results message
-                            $('.noResultsToShow').addClass('hideResultsMessage');
-                        }
-                    };
-                    parseItems.process = parseItemsToDisplay;
-                });
-
-
-
-
-                //   ***   Keyword Search   ***   //
-                $(function () {
-                    // scan the keyword each character the user inputs
-                    $('#keystroke_filter').on('keyup', function () {
-                        // Assign Search Key
-                        let keyword = $(this).val().toLowerCase();
-                        // filter the education abroad items for the input key
-                        $(function () {
-                            $('.lawFacultyWrapper').filter(function () {
-                                // when the search key is not present in the item then hide the item
-                                $(this).toggleClass('hideByText', !($(this).text().toLowerCase().indexOf(keyword) > -1));
-                            });
+            
+            //   ***   Process and Parse Visible Items   ***   //
+            $(function () {
+                let parseItemsToDisplay = function() {
+                    // assign array of currently visible content items
+                    visibleItems = $('.newsItemWrapper').not('.hideByDropdownCategories');
+                    // check to see if array is empty
+                    if (visibleItems.length == 0) {
+                        // when array is empty show the results message
+                        $('.noResultsToShow').removeClass('hideResultsMessage');
+                    } else {
+                        // when array has content items suppress the results message
+                        $('.noResultsToShow').addClass('hideResultsMessage');
+                    }
+                };
+                parseItems.process = parseItemsToDisplay;
+            });
+            
+            
+            
+            
+            //   ***   Keyword Search   ***   //
+            $(function () {
+                // scan the keyword each character the user inputs
+                $('#id_search').on('keyup', function () {
+                    // Assign Search Key
+                    let keyword = $(this).val().toLowerCase();
+                    // filter the items for the input key
+                    $(function () {
+                        $('.newsItem').filter(function () {
+                            // when the search key is not present in the item then hide the item
+                            $(this).toggleClass('hideByDropdownCategories', !($(this).text().toLowerCase().indexOf(keyword) > -1));
                         });
-                        // parse out unselected content items and limit display to user selected items
-                        parseItems.process();
                     });
+                    // parse out unselected content items and limit display to user selected items
+                    parseItems.process();
                 });
+            });
 
 
 
 
-
-                //   ***   Type Filter   ***   //
-                $(function () {
-                    // When the Dropdown Menu Selector Course Types Change - Execute change function
-                    $('form input:radio').change(function () {
-                        // Assign Search Key
-                        let typeKey = $(this).val();
-                        let viewAll = "All";
-                        console.log("typeKey: " + typeKey);
-                        // If Search Key is Not Null then Compare to the Type List Items in Each Content Item
-                        if (typeKey != viewAll) {
-                            $('.facultyStatus').filter(function (i, e) {
-                                var typeValue = $(this).text();
-                                console.log("typeValue: " + typeValue);
-                                // Check to see if the Key and Value are a Match
-                                if (typeValue.match(typeKey)) {
-                                    $(this).parents('.lawFacultyWrapper').removeClass('hideByType');
-                                } else {
-                                    $(this).parents('.lawFacultyWrapper').addClass('hideByType');
-                                }
-                            });
-                            // Else the Search Key is Null so Reset all Content Items to Visible
-                        } else {
-                            $('.lawFacultyWrapper').removeClass('hideByType');
-                        }
-                        // parse out unselected content items and limit display to user selected items
-                        parseItems.process();
-                    });
+            //   ***   Category Filter   ***   //
+            $(function () {
+                // When the Dropdown Menu Selector Course Types Change - Execute change function
+                $('#selectboxCategories').change(function () {
+                    // Assign Search Key
+                    let typeKey = $(this).val();
+                    // If Search Key is Not Null then Compare to the Type List Items in Each Content Item
+                    if (typeKey) {
+                        // search tags in each item
+                        $('ul.categories').filter(function (i, e) {
+                            var typeValue = $(this).text();
+                            // Check to see if the Key and Value are a Match
+                            if (typeValue.match(typeKey)) {
+                                $(this).parents('.newsItemWrapper').removeClass('hideByDropdownCategories');
+                            } else {
+                                $(this).parents('.newsItemWrapper').addClass('hideByDropdownCategories');
+                            }
+                        });
+                        // Else the Search Key is Null so Reset all Content Items to Visible
+                    } else {
+                        $('.newsItemWrapper').removeClass('hideByDropdownCategories');
+                    }
+                    // parse out unselected content items and limit display to user selected items
+                    parseItems.process();
                 });
+            });
 
 
 
 
-            }, 10);
-        });
+        }, 10);
     });
+});
 </script>
