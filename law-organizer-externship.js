059fa4d3db68d3ddd991631c4b4549804f4a8dfe
Vic Chimenti <victor.chimenti@gmail.com>
2021-05-12 10:57:46 -0700

    add px 2 to title and bodfy

--- law-organizer-externship.js
+++ law-organizer-externship.js
@@ -45,7 +45,7 @@
      * */
     var titleLink = '<h3 class="card-title visually-hidden">No Title Provided</h3>';
     var summary = '<p class="card-text visually-hidden subject location">No Subject or Location Provided</p>';
-    var openCardBody = '<div class="card-body">';
+    var openCardBody = '<div class="card-body px-2">';
     var closeCardBody = '</div>';
     var openHiddenFields = '<div class="visually-hidden">';
     var closeHiddenFields = '</div>';
@@ -64,7 +64,7 @@
      * 
      * */
     if (agency != "") {
-        titleLink = '<h3 class="card-title agency"><a href="' + fullTextLink + '" class="card-link" title="View full ' + agency + ' profile">' + agency + '</a></h3>';
+        titleLink = '<h3 class="card-title agency px-2"><a href="' + fullTextLink + '" class="card-link" title="View full ' + agency + ' profile">' + agency + '</a></h3>';
     }
 
 
@@ -82,7 +82,7 @@
         summary = '<p class="card-text subject location">' + subject + '</p>';
     } else {
         // When summary p is visually hidden compenstate for bottom margin in the card body
-        openCardBody = '<div class="card-body mb-4">';
+        openCardBody = '<div class="card-body mb-4 px-2">';
     }
 
 
