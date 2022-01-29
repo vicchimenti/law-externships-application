63f31308f7f36258c21b1ffe36829f825e434a3e
Vic Chimenti <victor.chimenti@gmail.com>
2022-01-29 11:26:36 -0800

    Deadline

--- law-fulltext.js
+++ law-fulltext.js
@@ -35,7 +35,10 @@ try {
 
     var materials = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Materials' output='normal' modifiers='striptags,htmlentities' />");
 
-    
+    var deadline = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Deadline' output='normal' modifiers='striptags,htmlentities' />");
+
+
+
     var lastModifiedDate = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='last_modified' format='EEEE, MMMM d, yyyy' />");
     var publishDate = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='publish_date' format='EEEE, MMMM d, yyyy' />");
     var contentId = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='content_id' />");
