5130b37caf6ebdde8e07f53be62cef54d720c2a2
Vic Chimenti <victor.chimenti@gmail.com>
2022-05-25 11:43:57 -0700

    ur

--- insert-tableAgencies.sql
+++ insert-tableAgencies.sql
@@ -1,10 +1,28 @@
 INSERT INTO tblAgencies (an, ag, ad, ci, st, co, zi, ur, ac, lo, sp, sv, de, ma, hr, hs, pr, ju, ty, re, pe, ss)
 SELECT
-   MY_XML.Customer.query('Document').value('.', 'VARCHAR(20)'),
-   MY_XML.Customer.query('Name').value('.', 'VARCHAR(50)'),
-   MY_XML.Customer.query('Address').value('.', 'VARCHAR(50)'),
-   MY_XML.Customer.query('Profession').value('.', 'VARCHAR(50)')
+    MY_XML.tblAgencies.query('an').value('.', 'INT'),
+    MY_XML.tblAgencies.query('ag').value('.', 'NVARCHAR(max)'),
+    MY_XML.tblAgencies.query('ad').value('.', 'NVARCHAR(256)'),
+    MY_XML.tblAgencies.query('ci').value('.', 'NVARCHAR(128)'),
+    MY_XML.tblAgencies.query('ci').value('.', 'NVARCHAR(128)'),
+    MY_XML.tblAgencies.query('zi').value('.', 'NVARCHAR (20)'),
+    MY_XML.tblAgencies.query('ur').value('.', 'NVARCHAR (256)'),
+	ur NVARCHAR (256),
+	ac NVARCHAR (128),
+	lo NVARCHAR (256),
+	sp NVARCHAR (256),
+	sv NVARCHAR (256),
+	de NVARCHAR (max),
+	ma NVARCHAR (256),
+	hr NVARCHAR (128),
+	hs NVARCHAR (128),
+	pr NVARCHAR (256),
+	ju NVARCHAR (256),
+	ty NVARCHAR (256),
+	re NVARCHAR (128),
+	pe NVARCHAR (max),
+
 FROM (SELECT CAST(MY_XML AS xml)
-      FROM OPENROWSET(BULK 'C:\Users\Victo\OneDrive - Seattle University\Marcom\Law\Externships\XML Folder\20220520\lawExternshipsActive.xml', SINGLE_BLOB) AS T(MY_XML)) AS T(MY_XML)
-      CROSS APPLY MY_XML.nodes('dataroot/tblAgencies') AS MY_XML (tblAgencies);
+        FROM OPENROWSET(BULK 'C:\Users\Victo\OneDrive - Seattle University\Marcom\Law\Externships\XML Folder\20220520\lawExternshipsActive.xml', SINGLE_BLOB) AS T(MY_XML)) AS T(MY_XML)
+        CROSS APPLY MY_XML.nodes('dataroot/tblAgencies') AS MY_XML (tblAgencies);
       
