741898774dff897d558484dd2fc4a1c73d9f329a
Vic Chimenti <victor.chimenti@gmail.com>
2022-05-25 11:48:19 -0700

    hr

--- insert-tableAgencies.sql
+++ insert-tableAgencies.sql
@@ -13,9 +13,8 @@ SELECT
     MY_XML.tblAgencies.query('sv').value('.', 'NVARCHAR(256)'),
     MY_XML.tblAgencies.query('de').value('.', 'NVARCHAR(max)'),
     MY_XML.tblAgencies.query('ma').value('.', 'NVARCHAR(256)'),
+    MY_XML.tblAgencies.query('hr').value('.', 'NVARCHAR(128)'),
 
-	ma NVARCHAR (256),
-	hr NVARCHAR (128),
 	hs NVARCHAR (128),
 	pr NVARCHAR (256),
 	ju NVARCHAR (256),
